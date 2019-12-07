9df1d919890251ccfbf9333168c54118325939b7
ikychant <iki.informasi@gmail.com>
2019-12-07 21:27:33 +0700

    Adding Function and repair variable A to rectangle

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 print("Hello Math World")
 
-# Calculate the area of a rectangle (A)
+# Calculate the area of a rectangle (rectangle)
 b = 10
 h = 10
-A = b * h
-print(A)
+rectangle = b * h
+print(rectangle)
 
 # Conditional Statement
-if A < 100:
+if rectangle < 100:
     print("Small")
-elif A == 100:
+elif rectangle == 100:
     print("Perfect")
 else:
     print("Large")
 
 # Loop
-print('with for')
+print("with for")
 for i in range(0, 10):
-    print(i+1, A)
+    print(i + 1, rectangle)
+
+# Function Based
+def function_area_of_rectangle(rectangle, b, h):
+    print(rectangle)
+    return b * h
+
+print(function_area_of_rectangle("r", 10, 4))
