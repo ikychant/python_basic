5984db405168b661856e339cf45c689f2960580a
ikychant <iki.informasi@gmail.com>
2019-12-07 21:11:06 +0700

    Conditional Statement

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 print("Hello Math World")
 
 # Calculate the area of a rectangle (A)
-b = 3
-h = 4
+b = 10
+h = 10
 A = b * h
 print(A)
+
+# Conditional Statement
+if A < 100:
+    print("Small")
+elif A == 100:
+    print("Perfect")
+else:
+    print("Large")
